0814d59ae140d0cc20c0ea2ddfd9049592937bea
Nguyễn Trọng Đại <neolizard1149@gmail.com>
2025-12-17 20:46:13 +0700

    Update edit.php

--- phong/edit.php
+++ phong/edit.php
@@ -1,7 +1,7 @@
 <?php include "../menu.php"; ?>
 <html>
 	<head>
-		<title>Bài tập xây dựng trang tìm kiếm với PHP & MySQL</title>
+		<title>Thao tác với danh sách phòng</title>
 		<meta charset = 'utf-8'>
 	   
 	</head>
